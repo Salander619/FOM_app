6ac2f6173e551722a7598c000960438924bc4f7d
Salander619 <come.badet@laposte.net>
2024-03-15 16:17:07 +0100

    modification name without _

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='FOM_app',
+    name='appFOM',
     version='0.1.0',
     description='Common file of the application FOM',
     url='https://github.com/Salander619/FOM_app.git',
     author='Come Badet',
     author_email='come.badet@apc.in2p3.fr',
     license='',
-    packages=find_packages('FOM_app'),
+    packages=find_packages('appFOM'),
     install_requires=['numpy',
                       'lisaconstants',
                       'matplotlib',
