812e760a1e5e183dc3b7406fba00ab314f5d479a
Salander619 <come.badet@laposte.net>
2024-03-15 15:20:34 +0100

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='FOM_app',
+    version='0.1.0',  
+    description='Common file of the application FOM',
+    url='https://github.com/Salander619/FOM_app.git',
+    author='Come Badet',
+    author_email='come.badet@apc.in2p3.fr',
+    license='',
+    packages=['FOM_app'],
+    install_requires=['numpy',
+                      'lisaconstants',
+                      'matplotlib',
+                      'scipy',
+                      'lisaorbits',
+                      'fastgb'],
+
+    classifiers=[],
+)
