e7dded2cea79a49e7ecab624f7896aea4545fc83
Salander619 <come.badet@laposte.net>
2024-04-03 12:15:28 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,6 +23,7 @@ release = '0.1'
 extensions = [
     'sphinx.ext.duration',
     'sphinx.ext.autodoc',
+    'furo',
 ]
 
 templates_path = ['_templates']
@@ -33,5 +34,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = ['furo']
+html_theme = 'furo'
 html_static_path = ['_static']
